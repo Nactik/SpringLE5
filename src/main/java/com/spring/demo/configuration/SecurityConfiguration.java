dc346272c4cc5e9cdd272984648d3723e9b90dc8
Nathan Capiaux <capiauxnathan@gmail.com>
2021-09-24 14:18:08 +0200

    add mass truc jsp

---
+++ src/main/java/com/spring/demo/configuration/SecurityConfiguration.java
@@ -0,0 +1,36 @@
+package com.spring.demo.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+        manager.createUser(User.withDefaultPasswordEncoder().username("demo").password("demo").roles("USER").build());
+        manager.createUser(User.withDefaultPasswordEncoder().username("admin").password("admin").roles("admin").build());
+        return manager;
+    }
+
+    public void configure(HttpSecurity http) throws Exception{
+        http
+                .authorizeRequests()
+                .antMatchers("/robots.txt").permitAll()
+                .antMatchers("/webjars/**").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login").permitAll().defaultSuccessUrl("/teachers")
+                .and()
+                .logout().permitAll();
+    }
+}

c30281401e1b88ba8bbdad62f00f4bf4606da675
Nathan Capiaux <capiauxnathan@gmail.com>
2021-09-24 14:29:04 +0200

    add security checks

--- src/main/java/com/spring/demo/configuration/SecurityConfiguration.java
+++ src/main/java/com/spring/demo/configuration/SecurityConfiguration.java
@@ -2,6 +2,7 @@ package com.spring.demo.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -26,11 +27,19 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .authorizeRequests()
                 .antMatchers("/robots.txt").permitAll()
                 .antMatchers("/webjars/**").permitAll()
+                .antMatchers("/api/**").permitAll()
+                .antMatchers("/h2/**").hasRole("ADMIN")
+                .antMatchers("/swagger-ui.html").hasRole("Admin")
+                .antMatchers(HttpMethod.POST,"/teachers/create").hasRole("ADMIN")
+                .antMatchers(HttpMethod.POST,"/teachers/{\\d+}").hasRole("ADMIN")
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
                 .loginPage("/login").permitAll().defaultSuccessUrl("/teachers")
                 .and()
                 .logout().permitAll();
+
+        //h2
+        http.headers().frameOptions().disable().and().csrf().disable();
     }
 }
